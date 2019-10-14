39662460d78244c593d8a413eda9fd4892960ea6
tangocthao <tangocthao@gmail.com>
2019-10-14 11:55:26 +0700

    Update shortSummary.js
    update

--- src/components/shortSummary.js
+++ src/components/shortSummary.js
@@ -4,7 +4,7 @@ import Person from './Person';
 class shortSummary extends React.Component{
     render(){
         const {data} = this.props;
-        return(
+        return (
             <React.Fragment>
                 <h2>{data.title}</h2>
                 <Person data={data.Person} />
